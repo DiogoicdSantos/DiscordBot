04b52ab8054ad1716128d218bca2f4ce6e7c1a7d
diogosantosicd@outlook.com <diogosantosicd@outlook.com>
2021-02-06 19:34:50 +0000

    changes

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 require('dotenv').config()
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DISCORD_TOKEN;
+const prefix = '!'
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('The bot is online!');
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    }
 });
 
 client.login(token);
