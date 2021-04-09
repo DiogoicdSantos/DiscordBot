af950f551e0478b66c6b8c915dd7e0a80c4e2221
diogosantosicd@outlook.com <diogosantosicd@outlook.com>
2021-04-09 16:08:16 +0100

    Changes

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,14 @@
 module.exports = {
-    name:'ping',
-    description:'this is a ping command!',
-    execute(message, args){
-        message.channel.send('pong!');
-    }
-}
+  name: "ping",
+  description: "this is a ping command!",
+  async execute(message, args, client) {
+    var resMsg = await message.channel.send(
+      "Ping is being appreciated... :bar_chart:"
+    );
+    resMsg.edit(
+      "Ping: " +
+        Math.round(resMsg.createdTimestamp - message.createdTimestamp) +
+        " ms."
+    );
+  },
+};
--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-require('dotenv').config()
-const fs = require('fs');
-const Discord = require('discord.js');
-const client = new Discord.Client({partials:["MESSAGE","CHANNEL","REACTION"]});
+require("dotenv").config();
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 const token = process.env.DISCORD_TOKEN;
-const prefix = '!'
+const prefix = "!";
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-    console.log('The bot is online!');
+client.once("ready", () => {
+  console.log("The bot is online!");
 });
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    if(command === 'ping'){client.commands.get('ping').execute(message, args);}
-    else if(command === 'cat'){client.commands.get('cat').execute(message, args);}
-    else if(command === 'ban'){client.commands.get('ban').execute(message, args);}
-    else if(command === 'kick'){client.commands.get('kick').execute(message, args);}
-    else if(command === 'reaction'){client.commands.get('reaction').execute(message, args, Discord, client);}
+  if (command === "ping") {
+    client.commands.get("ping").execute(message, args);
+  } else if (command === "cat") {
+    client.commands.get("cat").execute(message, args);
+  } else if (command === "ban") {
+    client.commands.get("ban").execute(message, args);
+  } else if (command === "kick") {
+    client.commands.get("kick").execute(message, args);
+  } else if (command === "reaction") {
+    client.commands.get("reaction").execute(message, args, Discord, client);
+  }
 });
 
 client.login(token);
